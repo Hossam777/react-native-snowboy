4a6da77ad9ec668d24eb3813d43fae7c5c0d1517
Hossam777 <31036061+Hossam777@users.noreply.github.com>
2020-05-31 12:14:08 +0200

    added event listner

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-import { NativeModules } from 'react-native';
-
-const { Snowboy } = NativeModules;
-
-export default Snowboy;
+var DeviceEventEmitter = require('react-native').DeviceEventEmitter; 
+var SnowBoy = require('react-native').NativeModules.Snowboy;
+var Snowboy = {
+	addEventListener(eventName,handler) {
+		DeviceEventEmitter.addListener(eventName, handler)
+	},
+	removeEventListener(eventName,handler) {
+		DeviceEventEmitter.removeListener(eventName, handler)
+	},
+	startRecording() {
+		SnowBoy.start();
+	},
+	stopRecording() {
+		SnowBoy.stop();
+	},
+	initHotword() {
+		return SnowBoy.initHotword();
+	},
+	destroy() {
+		SnowBoy.destroy();
+	}
+}
+module.exports = Snowboy;
