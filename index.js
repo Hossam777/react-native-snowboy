304b9001066469fde90ac8b12cacd5ba5e2cc8e3
Hossam <alexmercerjamesheller@gmail.com>
2020-07-26 17:45:56 +0200

    remove all listners added

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-var DeviceEventEmitter = require('react-native').DeviceEventEmitter; 
+var DeviceEventEmitter = require('react-native').DeviceEventEmitter;
 var SnowBoy = require('react-native').NativeModules.Snowboy;
 var Snowboy = {
-	addEventListener(eventName,handler) {
-		DeviceEventEmitter.addListener(eventName, handler)
-	},
-	removeEventListener(eventName,handler) {
-		DeviceEventEmitter.removeListener(eventName, handler)
-	},
-	startRecording() {
-		SnowBoy.start();
-	},
-	stopRecording() {
-		SnowBoy.stop();
-	},
-	initHotword() {
-		return SnowBoy.initHotword();
-	},
-	destroy() {
-		SnowBoy.destroy();
-	}
+    addEventListener(eventName, handler) {
+        DeviceEventEmitter.addListener(eventName, handler)
+    },
+    removeEventListener(eventName, handler) {
+        DeviceEventEmitter.removeListener(eventName, handler)
+    },
+    removeAllListeners(eventType) {
+        DeviceEventEmitter.removeAllListeners(eventType)
+    },
+    startRecording() {
+        SnowBoy.start();
+    },
+    stopRecording() {
+        SnowBoy.stop();
+    },
+    initHotword() {
+        return SnowBoy.initHotword();
+    },
+    destroy() {
+        SnowBoy.destroy();
+    }
 }
 module.exports = Snowboy;
